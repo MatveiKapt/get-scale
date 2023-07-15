0264b4358ec0a044bd78efc28ad7c839c9a60764
Matvei Kaptiug <matvei_1906@mail.ru>
2023-07-15 14:42:14 +0300

    Пробую пофиксить отображение в старых браузерах

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { getRandomScale } from './data.js';
 import { changeScaleElementValue } from './view.js';
 
+const pageBody = document.querySelector('body');
+pageBody.style.height = window.innerHeight + 'px';
+
 const button = document.querySelector('.interface__button');
 
 const onButtonClick = () => {

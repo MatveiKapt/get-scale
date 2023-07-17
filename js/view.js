aec18ce20ab7368df643f266594e8b9a06a25f9f
Matvei Kaptiug <matvei_1906@mail.ru>
2023-07-17 21:54:33 +0300

    Добавляет анимацию шейка для шара

--- js/view.js
+++ js/view.js
@@ -1,7 +1,17 @@
+const scaleElementWrapper = document.querySelector('.interface__scale-text-wrapper');
 const scaleElement = document.querySelector('.interface__scale-text');
 
 const changeScaleElementValue = (value) => {
-  scaleElement.textContent = value;
+  scaleElementWrapper.classList.add('shake');
+
+  setTimeout(() => {
+    scaleElementWrapper.classList.remove('shake');
+  }, 200);
+
+  setTimeout(() => {
+    scaleElement.textContent = value
+  }, 200);
+
 };
 
 export { changeScaleElementValue };
